defeec4c67b758c94fb1a03ff25723e41a6d1fdc
Yeastplume <yeastplume@gmail.com>
2024-06-04 11:31:00 +0100

    move wallet client to image directory

---
+++ base-olivetin/owner_api_client/src/create_wallet.js
@@ -0,0 +1,23 @@
+const ownerClient = require('./owner_api_client.js');
+
+const port = process.env.OWNER_API_PORT || 3415;
+
+async function main() {
+	let client = ownerClient.initClient('http://host.docker.internal:' + port + '/v3/owner');
+	let shared_key = await ownerClient.initSecure();
+
+	let response = await new ownerClient.JSONRequestEncrypted(1, 'create_wallet', {
+		"name": null,
+		"mnemonic": null,
+		"mnemonic_length": 32,
+		"password": "password"
+	}).send(shared_key);
+
+	console.log("Response: ", response);
+}
+
+
+
+main();
+
+
