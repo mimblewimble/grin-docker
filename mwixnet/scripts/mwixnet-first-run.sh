68c1e6acae36d8472f147fd5dfe1005b772eb02e
yeastplume <yeastplume@protonmail.com>
2024-07-19 11:16:51 +0100

    changes to wallet password, tor runtime setup

--- mwixnet/scripts/mwixnet-first-run.sh
+++ mwixnet/scripts/mwixnet-first-run.sh
@@ -14,15 +14,17 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     else
         mwixnet init-config
     fi
-    GRIN_NODE_URL_REPLACED=$(echo "$GRIN_NODE_URL" | sed 's/\//\\\//g')
-    GRIN_WALLET_URL_REPLACED=$(echo "$WALLET_OWNER_URL" | sed 's/\//\\\//g')
-    sed -i "s/.*grin_node_url.*/grin_node_url = \"$GRIN_NODE_URL_REPLACED\"/g" /home/grinuser/.grin/$GRIN_CHAIN_TYPE/mwixnet-config.toml
-    sed -i "s/.*wallet_owner_url.*/wallet_owner_url = \"$GRIN_WALLET_URL_REPLACED\"/g" /home/grinuser/.grin/$GRIN_CHAIN_TYPE/mwixnet-config.toml
-    sed -i "s/.*grin_node_secret_path.*/#grin_node_secret_path = \"$GRIN_WALLET_URL_REPLACED\"/g" /home/grinuser/.grin/$GRIN_CHAIN_TYPE/mwixnet-config.toml
 fi
 
+GRIN_NODE_URL_REPLACED=$(echo "$GRIN_NODE_URL" | sed 's/\//\\\//g')
+GRIN_WALLET_URL_REPLACED=$(echo "$WALLET_OWNER_URL" | sed 's/\//\\\//g')
+sed -i "s/.*grin_node_url.*/grin_node_url = \"$GRIN_NODE_URL_REPLACED\"/g" /home/grinuser/.grin/$GRIN_CHAIN_TYPE/mwixnet-config.toml
+sed -i "s/.*wallet_owner_url.*/wallet_owner_url = \"$GRIN_WALLET_URL_REPLACED\"/g" /home/grinuser/.grin/$GRIN_CHAIN_TYPE/mwixnet-config.toml
+sed -i 's/.*grin_node_secret_path.*/grin_node_secret_path = "\/grin-node-home\/.grin\/test\/.foreign_api_secret"/g' /home/grinuser/.grin/$GRIN_CHAIN_TYPE/mwixnet-config.toml
+sed -i 's/.*wallet_owner_secret_path.*/#wallet_owner_secret_path = "\/grin-wallet-home\/.grin\/test\/.owner_api_secret"/g' /home/grinuser/.grin/$GRIN_CHAIN_TYPE/mwixnet-config.toml
+
 if [ $GRIN_CHAIN_TYPE == "test" ]; then
-    mwixnet --testnet
+    RUST_BACKTRACE=full mwixnet --testnet --wallet_pass=$WALLET_PASSWORD
 else
-    mwixnet
+    mwixnet --wallet_pass=$WALLET_PASSWORD
 fi
