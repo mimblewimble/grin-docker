80da6a8044a4320516580f28617ea9d9932260ad
yeastplume <yeastplume@protonmail.com>
2024-08-07 15:29:32 +0100

    update wallet init process to happen automatically on first startup, (without password)

--- wallet/scripts/wallet-first-run.sh
+++ wallet/scripts/wallet-first-run.sh
@@ -12,6 +12,15 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     fi
 fi
 
+GRIN_NODE_URL_REPLACED=$(echo "$NODE_API_HTTP_ADDR" | sed 's/\//\\\//g')
+NODE_API_SECRET_PATH_REPLACED=$(echo "$NODE_API_SECRET_PATH" | sed 's/\//\\\//g')
+sed -i "s/.*check_node_api_http_addr.*/check_node_api_http_addr = \"$GRIN_NODE_URL_REPLACED\"/g" /home/grinuser/.grin/$GRIN_CHAIN_TYPE/grin-wallet.toml
+sed -i "s/.*node_api_secret_path.*/node_api_secret_path = \"$NODE_API_SECRET_PATH_REPLACED\"/g" /home/grinuser/.grin/$GRIN_CHAIN_TYPE/grin-wallet.toml
+sed -i "s/.*owner_api_listen_port.*/owner_api_listen_port = $OWNER_API_LISTEN_PORT/g" /home/grinuser/.grin/$GRIN_CHAIN_TYPE/grin-wallet.toml
+sed -i "s/api_listen_port.*/api_listen_port = $WALLET_API_LISTEN_PORT/g" /home/grinuser/.grin/$GRIN_CHAIN_TYPE/grin-wallet.toml
+# hack to avoid complicated sed command
+sed -i "s/.*#tls_certificate_key.*/owner_api_listen_interface = \"$OWNER_API_LISTEN_INTERFACE\"/g" /home/grinuser/.grin/$GRIN_CHAIN_TYPE/grin-wallet.toml
+
 if [ $GRIN_CHAIN_TYPE == "test" ]; then
     RUST_BACKTRACE=full grin-wallet --testnet owner_api --run_foreign
 else
